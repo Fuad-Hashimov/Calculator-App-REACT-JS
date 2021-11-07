2241b13f5bc125d7911f1e0bcd10306db3f6e2d7
FuadHashimov91 <fuadbh@code.edu.az>
2021-11-07 04:43:54 +0400

    Add Operators Com

--- calculatorapp/src/App.js
+++ calculatorapp/src/App.js
@@ -1,25 +1,20 @@
 import "./App.css";
 import Digits from "./Components/Digits";
+import Operators from "./Components/Operators";
 
 function App() {
   return (
     <div className="App">
-      <section className='calculator'> 
-        <div className='display'>
+      <section className="calculator">
+        <div className="display">
           <span>(0)</span>0
         </div>
-        <div className='operators'>
-          <button>/</button>
-          <button>*</button>
-          <button>+</button>
-          <button>-</button>
-          <button>DEL</button>
-        </div>
-        <div className='digits'>
-            <Digits />
-            <button>0</button>
-            <button>.</button>
-            <button>=</button>
+        <Operators />
+        <div className="digits">
+          <Digits />
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
         </div>
       </section>
     </div>
---
+++ calculatorapp/src/Components/Operators.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Operators() {
+  return (
+    <div className="operators">
+      <button>/</button>
+      <button>*</button>
+      <button>+</button>
+      <button>-</button>
+      <button>DEL</button>
+    </div>
+  );
+}
+export default Operators;
